a475be4d90bd4b4f16c2f704fad83024e96b9529
ArthurPai <baidragoon@gmail.com>
2014-06-26 17:55:39 +0800

    set root path link in nav bar

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Myflix::Application.routes.draw do
   get 'ui(/:action)', controller: 'ui'
 
+  root to: 'videos#index'
   get '/home', to: 'videos#index'
 
   resources :videos, only: [:index, :show]
