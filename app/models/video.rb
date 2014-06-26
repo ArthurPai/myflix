fe40f7ebea2f3bab97326055a76d82d43fb527e3
ArthurPai <baidragoon@gmail.com>
2014-06-26 16:06:47 +0800

    Add categories, add association with videos

--- app/models/video.rb
+++ app/models/video.rb
@@ -1,3 +1,5 @@
 class Video < ActiveRecord::Base
   validates :title, presence: true
+
+  belongs_to :category
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,19 +7,33 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 Video.destroy_all
+Category.destroy_all
 
-Video.create(
-    [
-        {
-            title: 'monk',
-            description: 'Pizza boy Philip J. Fry awakens in the 31st century after 1,000 years of cryogenic preservation in this animated series. After he gets a job at an interplanetary delivery service, Fry embarks on ridiculous escapades to make sense of his predicament.',
-            small_cover_url: '/tmp/monk.jpg',
-            large_cover_url: '/tmp/monk_large.jpg'
-        },
-        {
-            title: 'monk',
-            description: 'Pizza boy Philip J. Fry awakens in the 31st century after 1,000 years of cryogenic preservation in this animated series. After he gets a job at an interplanetary delivery service, Fry embarks on ridiculous escapades to make sense of his predicament.',
-            small_cover_url: '/tmp/monk.jpg'
-        },
-    ]
-)
+commedies = Category.create(name: 'TV Commedies')
+dramas = Category.create(name: 'TV Dramas')
+reality = Category.create(name: 'Reality TV')
+
+monk = Video.create(
+    title: 'monk',
+    description: 'Pizza boy Philip J. Fry awakens in the 31st century after 1,000 years of cryogenic preservation in this animated series. After he gets a job at an interplanetary delivery service, Fry embarks on ridiculous escapades to make sense of his predicament.',
+    small_cover_url: '/tmp/monk.jpg',
+    large_cover_url: '/tmp/monk_large.jpg')
+
+futurama = Video.create(
+    title: 'Futurama',
+    description: 'Pizza boy Philip J. Fry awakens in the 31st century after 1,000 years of cryogenic preservation in this animated series. After he gets a job at an interplanetary delivery service, Fry embarks on ridiculous escapades to make sense of his predicament.',
+    small_cover_url: '/tmp/futurama.jpg')
+
+south_park = Video.create(
+    title: 'South Park',
+    description: 'Pizza boy Philip J. Fry awakens in the 31st century after 1,000 years of cryogenic preservation in this animated series. After he gets a job at an interplanetary delivery service, Fry embarks on ridiculous escapades to make sense of his predicament.',
+    small_cover_url: '/tmp/south_park.jpg')
+
+family_guy = Video.create(
+    title: 'Family Guy',
+    description: 'Pizza boy Philip J. Fry awakens in the 31st century after 1,000 years of cryogenic preservation in this animated series. After he gets a job at an interplanetary delivery service, Fry embarks on ridiculous escapades to make sense of his predicament.',
+    small_cover_url: '/tmp/family_guy.jpg')
+
+commedies.videos << monk
+dramas.videos << futurama
+reality.videos << south_park << family_guy
