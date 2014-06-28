187b21538c098413348f21cc84450049b7bf6bf6
ArthurPai <baidragoon@gmail.com>
2014-06-28 08:27:32 +0800

    Implement search by title feature of video model

--- app/models/video.rb
+++ app/models/video.rb
@@ -3,4 +3,9 @@ class Video < ActiveRecord::Base
   validates :description, presence: true
 
   belongs_to :category
+
+  def self.search_by_title(search_title)
+    return [] if search_title.blank?
+    where("title LIKE ?", "%#{search_title}%").order('created_at desc')
+  end
 end
--- spec/models/video_spec.rb
+++ spec/models/video_spec.rb
@@ -5,4 +5,62 @@ describe Video do
   it { should belong_to(:category) }
   it { should validate_presence_of(:title) }
   it { should validate_presence_of(:description) }
+
+  describe 'search by title' do
+    it 'returns an empty array if there is no match' do
+      monk = Video.create(title: 'Monk', description: 'Grate Video!')
+      monkey = Video.create(title: 'Monkey', description: 'It is funny!')
+      south_park = Video.create(title: 'South Park', description: 'So funny!')
+
+      expect(Video.search_by_title('Transformer')).to eq([])
+    end
+
+    it 'returns an array of one video for an exact match' do
+      monk = Video.create(title: 'Monk', description: 'Grate Video!')
+      monkey = Video.create(title: 'Monkey', description: 'It is funny!')
+      south_park = Video.create(title: 'South Park', description: 'So funny!')
+
+      expect(Video.search_by_title('South Park')).to eq([south_park])
+    end
+
+    it 'returns an array of one video for a partial match' do
+      monk = Video.create(title: 'Monk', description: 'Grate Video!')
+      monkey = Video.create(title: 'Monkey', description: 'It is funny!')
+      south_park = Video.create(title: 'South Park', description: 'So funny!')
+
+      expect(Video.search_by_title('South')).to eq([south_park])
+    end
+
+    it 'returns an array of one video for case sensitive match' do
+      monk = Video.create(title: 'Monk', description: 'Grate Video!')
+      monkey = Video.create(title: 'Monkey', description: 'It is funny!')
+      south_park = Video.create(title: 'South Park', description: 'So funny!')
+
+      expect(Video.search_by_title('south')).to eq([south_park])
+    end
+
+    it 'returns an array of all matches order by create time' do
+      monk = Video.create(title: 'Monk', description: 'Grate Video!', created_at: 1.days.ago)
+      monkey = Video.create(title: 'Monkey', description: 'It is funny!')
+      south_park = Video.create(title: 'South Park', description: 'So funny!')
+
+      expect(Video.search_by_title('Monk')).to eq([monkey, monk])
+    end
+
+    it 'returns an empty array for a search with an empty string' do
+      monk = Video.create(title: 'Monk', description: 'Grate Video!')
+      monkey = Video.create(title: 'Monkey', description: 'It is funny!')
+      south_park = Video.create(title: 'South Park', description: 'So funny!')
+
+      expect(Video.search_by_title('')).to eq([])
+    end
+
+    it 'returns an empty array for a search with an nil string' do
+      monk = Video.create(title: 'Monk', description: 'Grate Video!')
+      monkey = Video.create(title: 'Monkey', description: 'It is funny!')
+      south_park = Video.create(title: 'South Park', description: 'So funny!')
+
+      expect(Video.search_by_title(nil)).to eq([])
+    end
+  end
 end
