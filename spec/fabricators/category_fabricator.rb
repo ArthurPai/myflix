55cbc2f96945a5cc323aa5a3133f47957efa1404
ArthurPai <baidragoon@gmail.com>
2014-07-01 00:34:35 +0800

    fix category spec and fabricator

--- spec/fabricators/category_fabricator.rb
+++ spec/fabricators/category_fabricator.rb
@@ -1,3 +1,3 @@
 Fabricator(:category) do
-  name 'Comedies'
+  name { Faker::Lorem.word }
 end
--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -2,6 +2,7 @@ require 'spec_helper'
 
 describe Category do
   it { should have_many(:videos).order('title asc') }
+  it { should validate_presence_of(:name) }
 
   describe 'recent videos' do
     let!(:cat) { Fabricate(:category) }
