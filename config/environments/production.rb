f702c77c2eb12e9d45554cbada5709b1d71eff32
ArthurPai <baidragoon@gmail.com>
2014-07-09 15:18:55 +0800

    setup heroku with mailgun

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -21,12 +21,11 @@ Myflix::Application.configure do
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.default_url_options = { host: 'arthurflix.herokuapp.com' }
   config.action_mailer.smtp_settings = {
-      address:              'smtp.gmail.com',
-      port:                 587,
+      address:              ENV['MAILGUN_SMTP_SERVER'],
+      port:                 ENV['MAILGUN_SMTP_PORT'],
       domain:               'arthurflix.herokuapp.com',
-      user_name:            ENV["GMAIL_USERNAME"],
-      password:             ENV["GMAIL_PASSWORD"],
+      user_name:            ENV['MAILGUN_SMTP_LOGIN'],
+      password:             ENV['MAILGUN_SMTP_PASSWORD'],
       authentication:       'plain',
-      enable_starttls_auto: true  }
-
+      enable_starttls_auto: true }
 end
