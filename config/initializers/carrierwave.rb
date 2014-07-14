418426930d1e84427b11a71fe6df0b67277eb401
ArthurPai <baidragoon@gmail.com>
2014-07-14 22:18:22 +0800

    fix amazon s3 region

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -11,7 +11,7 @@ CarrierWave.configure do |config|
         :provider               => 'AWS',                             # required
         :aws_access_key_id      => "#{ENV['AWS_ACCESS_KEY_ID']}",     # required
         :aws_secret_access_key  => "#{ENV['AWS_SECRET_ACCESS_KEY']}", # required
-        :region                 => 'Tokyo',                           # optional, defaults to 'us-east-1'
+        :region                 => 'ap-northeast-1',                  # optional, defaults to 'us-east-1'
     }
     config.fog_directory  = "#{ENV['AWS_DIRECTORY']}"               # required
     config.fog_public     = true                                    # optional, defaults to true
