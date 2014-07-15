774964815cc2d0725c360ac5511efeeea6eebbe0
ArthurPai <baidragoon@gmail.com>
2014-07-15 10:35:26 +0800

    fix the config of Carrierwave gem

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -2,9 +2,7 @@ CarrierWave.configure do |config|
   config.permissions = 0666
   config.directory_permissions = 0777
 
-  if Rails.env.development? || Rails.env.test?
-    config.storage = :file
-  else
+  if Rails.env.staging? || Rails.env.production?
     config.storage = :fog
 
     config.fog_credentials = {
@@ -16,5 +14,8 @@ CarrierWave.configure do |config|
     config.fog_directory  = "#{ENV['AWS_DIRECTORY']}"               # required
     config.fog_public     = true                                    # optional, defaults to true
     config.fog_attributes = {'Cache-Control'=>'max-age=315576000'}  # optional, defaults to {}
+  else
+    config.storage = :file
+    config.enable_processing = Rails.env.development?
   end
 end
