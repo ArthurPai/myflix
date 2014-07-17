3700cdb021b5bff2b687e3888942207af11ec203
ArthurPai <baidragoon@gmail.com>
2014-07-17 21:25:15 +0800

    fix carrierwave setting

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -18,20 +18,22 @@ CarrierWave.configure do |config|
     config.storage = :file
     config.enable_processing = Rails.env.development?
 
-    # make sure our uploader is auto-loaded
-    VideoLargeCoverUploader
-    VideoSmallCoverUploader
+    unless Rails.env.development?
+      # make sure our uploader is auto-loaded
+      VideoLargeCoverUploader
+      VideoSmallCoverUploader
 
-    # use different dirs when testing
-    CarrierWave::Uploader::Base.descendants.each do |klass|
-      next if klass.anonymous?
-      klass.class_eval do
-        def cache_dir
-          "#{Rails.root}/spec/support/uploads/tmp"
-        end
+      # use different dirs when testing
+      CarrierWave::Uploader::Base.descendants.each do |klass|
+        next if klass.anonymous?
+        klass.class_eval do
+          def cache_dir
+            "#{Rails.root}/spec/support/uploads/tmp"
+          end
 
-        def store_dir
-          "#{Rails.root}/spec/support/uploads/#{model.class.to_s.underscore}/#{model.id}"
+          def store_dir
+            "#{Rails.root}/spec/support/uploads/#{model.class.to_s.underscore}/#{model.id}"
+          end
         end
       end
     end
